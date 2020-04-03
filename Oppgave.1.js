0d200fe583914092b46a081a3e539254d49c9d3a
Vaughub <olakamfjord@gmail.com>
2020-04-03 20:18:52 +0200

    Initial commit

---
+++ Oppgave.1.js
@@ -0,0 +1,36 @@
+function isDateValid(date) {
+    var year = date.slice(6, 10);
+    var month = date.slice(3, 5);
+    var day = date.slice(0, 2);
+
+    if (dateLength(date) && dateDot(date) && dateYear(year) && dateMonth(month) && dateDay(day, month, year)) return 'Valid date';
+    return 'Invalid date';
+}
+
+function dateLength(date) {
+    return date.length == 10;
+}
+    
+function dateDot(date) {
+    return date.charAt(2) == '.' && date.charAt(5) == '.';
+}
+
+function dateYear(year) {
+    return year.length == 4 && year >= '0000' && year <= '9999';
+}
+
+function dateMonth(month) {
+    return month.length == 2 && month >= '01' && month <= '12';
+}
+
+function dateDay(day, month, year) {
+    if (day <= 28 && month == 02) return true;
+    if (day == 29 && month == 02 && isLeapYear(year)) return true;
+    if (day <= 30 && !(month == 02)) return true;
+    if (day == 31 && month.match(/^01|03|05|07|08|10|12$/)) return true;
+    return false;
+}
+
+function isLeapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
